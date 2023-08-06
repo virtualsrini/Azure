3c10271b0b03a38df683e56041cdec9ebb936dc7
Srinivasan <vasan22in@gmail.com>
2023-08-06 12:58:01 +0530

    del

--- Network Security Group/nsg.tf
+++ Network Security Group/nsg.tf
@@ -3,4 +3,18 @@ resource "azurerm_network_security_group" "ref_nsg" {
   name = "${var.prefix}-nsg"
   location = "${var.server_location}"
   resource_group_name = "${azurerm_resource_group.ref_rg.name}" 
+
+security_rule {
+  name = "RDP Inpound"
+  priority = "100"
+  direction = "Inbound"
+  access = "allow"
+  protocol = "TCP"
+  source_port_range = "3389"
+  destination_port_range = "*"
+  source_address_prefix = "*"
+  destination_address_prefix = "*"
+  resource_group_name = "${azurerm_resource_group.ref_rg.name}"
+  network_security_group_name = "${azurerm_network_security_group.ref_nsg.name}"
+  }
 }
